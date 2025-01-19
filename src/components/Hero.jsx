66c1c6d718e026fd6c22fb3245e72ad832896732
Ayan-Bhaumik <mrayanbhaumik@gmail.com>
2025-01-19 13:01:45 +0530

    recover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-        {/* <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col justify-center items-center mt-5">
 
   <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
         <div className="w-1 sm:h-80 h-40 violet-gradient" />
-        </div> */}
+        </div>
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
@@ -43,7 +43,7 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          {/* <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -55,7 +55,7 @@ const Hero = () => {
               }}
               className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
-          </div> */}
+          </div>
         </a>
       </div>
     </section>
